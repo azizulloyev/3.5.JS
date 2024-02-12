796df0a0971e00dc27bff461a35af35c74357f1b
azizulloyev <arabictoplegend@gmail.com>
2024-02-12 18:10:11 +0000

    homework 3.5.JS

---
+++ JS/main.js
@@ -0,0 +1,10 @@
+var a = prompt('yoshingizni yozing')
+    if (a == 15){
+        console.log('Dost')
+    }
+    else if (a < 15){
+        console.log('aka')
+    }
+    else if (a >15 ){
+        console.log('uka')
+    }
